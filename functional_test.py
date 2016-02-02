da88bf924204b7181261c569ee24123bf0b0f2e3
Pavle Batuta <pavledb@gmail.com>
2016-02-02 13:31:11 +0100

    FT now tests if we can input a to-do item.

--- functional_test.py
+++ functional_test.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -16,19 +18,48 @@ class NewVisitorTest(unittest.TestCase):
 
 		# Page title and header should mention to-do lists
 		self.assertIn('To-Do', self.browser.title)
-		self.fail('Finish the test!')
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do'. header_text)
 
 		# The user is invited to enter a to-do item immediately
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'), 
+			'Enter a to-do item'
+		)
+
 		# The user types in 'Buy peacock feathers' for some reason that I cannot fathom
+		inputbox.send_keys('Buy peacock feathers')
+
 		# The user hits enter.
+		inputbox.send_keys(Keys.ENTER)
+		
 		# The page updates
 		# Page now lists "1: Buy peacock feathers" as an item in a to-do list
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_element_by_id('tr')
+		self.assertTrue(
+			any(row.text == '1: Buy peacock feathers' for row in rows)
+		)
+
 		# There is still a text box for entering another item.
 		# The user enters: 'Use peacock feathers to make a fly'
-		#
+		inputbox.send_keys('Use peacock feathers to make a fly')
+
+		# The user hits enter
+		inputbox.send_keys(Keys.ENTER)
+
 		# The page updates again.
 		# Both items are present on the list.
-		#
+		self.assertTrue(
+			any(row.text == '1: Buy peacock feathers' for row in rows)
+		)
+		self.assertTrue(
+			any(row.text == '2: Use peacock feathers to make a fly' for row in rows)
+		)
+
+		self.fail('Finish the test!')
+
 		# The site has generated a unique URL for the user.
 		#
 		# The user goes to the great user place in the sky. Browser quits.
