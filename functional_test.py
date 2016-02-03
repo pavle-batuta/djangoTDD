bb2dd371b45e610a2537155eb26dede3d60bf63c
Pavle Batuta <pavledb@gmail.com>
2016-02-03 11:46:29 +0100

    Refactored FT

--- functional_test.py
+++ functional_test.py
@@ -12,7 +12,21 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
 	def test_can_start_a_list_and_retrieve_it_later(self):
+		test_strings = [
+			'Buy peacock feathers',
+			'Use peacock feathers to make a fly',
+		]
+		correct_strings = [
+			'1: Buy peacock feathers',
+			'2: Use peacock feathers to make a fly',
+		]
+		print(test_strings[0])
 		# Check out the homepage
 		self.browser.get('http://localhost:8000')
 
@@ -29,32 +43,26 @@ class NewVisitorTest(unittest.TestCase):
 		)
 
 		# The user types in 'Buy peacock feathers' for some reason that I cannot fathom
-		inputbox.send_keys('Buy peacock feathers')
+		inputbox.send_keys(test_strings[0])
 		# The user hits enter.
 		inputbox.send_keys(Keys.ENTER)
 		
 		# The page updates
 		# Page now lists "1: Buy peacock feathers" as an item in a to-do list
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		self.check_for_row_in_list_table(correct_strings[0])
+
 
 		# There is still a text box for entering another item.
 		# The user enters: 'Use peacock feathers to make a fly'
 		inputbox = self.browser.find_element_by_id('id_new_item')
-		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(test_strings[1])
 
 		# The user hits enter
 		inputbox.send_keys(Keys.ENTER)
 
 		# The page updates again.
 		# Both items are present on the list.
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-		self.assertIn(
-			'2: Use peacock feathers to make a fly', [row.text for row in rows]
-		)
+		self.check_for_row_in_list_table(correct_strings[1])
 
 		self.fail('Finish the test!')
 
