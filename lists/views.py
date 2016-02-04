c38c947f16f41e45f00930d76f4d1004ce0ca535
Pavle Batuta <pavledb@gmail.com>
2016-02-04 11:37:36 +0100

    New URL for creating forms, with tests

--- lists/views.py
+++ lists/views.py
@@ -9,12 +9,12 @@ def home_page(request):
 	# NOTE: templates are expanded from the dictionary
 	# Get will return an empty string so that we don't break the webpage when
 	# we do not have a valid template expansion!
-	if request.method == 'POST':
-		Item.objects.create(text=request.POST['item_text'])
-		return redirect('/lists/the-only-list-in-the-world/')
-
 	return render(request, 'home.html', {'items': Item.objects.all()})
 
 def view_list(request):
 	items = Item.objects.all()
 	return render(request, 'list.html', {'items': items})
+
+def new_list(request):
+	Item.objects.create(text=request.POST['item_text'])
+	return redirect('/lists/the-only-list-in-the-world/')
