58723689693ef7c43c41107dc47f7b74989f6ec3
Pavle Batuta <pavledb@gmail.com>
2016-02-05 15:51:29 +0100

    Started using Bootstrap for layout

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -1,11 +1,11 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 import unittest
 import time
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 	"""Test case for a new visitor"""
 	def setUp(self):
 		self.browser = webdriver.Firefox()
